49aeb9424b56ae780fec37561d5b8c27bc5d8595
peeChulchul <eyeless356@naver.com>
2023-11-23 11:37:41 +0900

    refactor:사이드바 정리 , 숫자 포맷데이터함수 추가

---
+++ src/data/sidebar/accordion_data.js
@@ -0,0 +1,21 @@
+import { IoCalendar } from "react-icons/io5";
+import { FaMessage } from "react-icons/fa6";
+import { GiMuscleUp } from "react-icons/gi";
+
+export const accordionData = [
+  {
+    fill: "#33BFFF",
+    icon: <GiMuscleUp />,
+    title: "오운완"
+  },
+  {
+    fill: "#29CC39",
+    icon: <IoCalendar />,
+    title: "오식완"
+  },
+  {
+    fill: "#105EFB",
+    icon: <FaMessage />,
+    title: "팔로우"
+  }
+];
