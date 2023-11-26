9f7edc818238ab42bac2a92a3f5a74aa14a9bf3a
dosion9 <dosion9ood@gmail.com>
2023-11-26 17:38:42 +0900

    이미지가 여러개 일 때 변경할 수 있는 UI 만들기 #45

--- src/pages/detail_post/test.jsx
+++ src/pages/detail_post/test.jsx
@@ -2,67 +2,174 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Hashtag from "components/hashtag";
 import { v4 as uuid } from "uuid";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { GrPrevious, GrNext } from "react-icons/gr";
 function Test({ data }) {
   const { displayName, content, imgs, category, hashtag, postid, uid } = data;
-  const [currentImg, setCurrentImg] = useState(imgs[0]);
+  const [currentImgIndex, setCurrentImgIndex] = useState(0);
 
-  const onClickSubImg = (imgURL) => {
-    // setCurrentImg(imgURL)
-    console.log(imgURL);
+  const onClickSubImg = (index) => {
+    setCurrentImgIndex(index);
+  };
+
+  const showNextImg = (boolean) => {
+    if (boolean) {
+      const nextImgIndex = currentImgIndex + 1;
+      nextImgIndex >= imgs.length ? setCurrentImgIndex(0) : setCurrentImgIndex(nextImgIndex);
+    }
+    const preImgIndex = currentImgIndex - 1;
+    preImgIndex < 0 ? setCurrentImgIndex(imgs.length - 1) : setCurrentImgIndex(preImgIndex);
   };
 
   return (
     <StWrap>
-      <div className="row">nickname</div>
-      <StImgWrap>
-        <img src={currentImg.url} alt="" />
-      </StImgWrap>
-      <div className="row">
-        {imgs.map((n) => {
+      <StRow className="header">
+        <div>{displayName}</div>
+        <div>
+          <StIconWrap>
+            <MdDelete />
+          </StIconWrap>
+          <StIconWrap>
+            <FaEdit />
+          </StIconWrap>
+        </div>
+      </StRow>
+      <StRow className="center">
+        <StImgWrap backgroundImg={imgs[currentImgIndex].url}>
+          <img src={imgs[currentImgIndex].url} alt="" />
+
+          <StImgControler className="left" onClick={() => showNextImg(false)}>
+            <GrPrevious />
+          </StImgControler>
+          <StImgControler className="right" onClick={() => showNextImg(true)}>
+            <GrNext />
+          </StImgControler>
+        </StImgWrap>
+      </StRow>
+      <StRow className="center">
+        {imgs.map((n, i) => {
           return (
-            <StSubImgWrap onClick={() => onClickSubImg(n)}>
+            <StSubImgWrap className={currentImgIndex === i ? "active" : null} onClick={() => onClickSubImg(i)}>
               <img src={n.url} alt="" />
             </StSubImgWrap>
           );
         })}
-      </div>
-      <div className="row">
+      </StRow>
+
+      <StRow>
         <Hashtag hashtag={"true"} content={category} color={"white"} fontColor={"base"} />
         {hashtag.map((n) => {
-          <Hashtag hashtag={"true"} content={n} color={"base"} fontColor={"white"} key={uuid()} />;
+          return <Hashtag hashtag={"true"} content={n} color={"base"} fontColor={"white"} key={uuid()} />;
         })}
-      </div>
-      <div className="row">
+      </StRow>
+      <StRow>
         <p>{content}</p>
-      </div>
+      </StRow>
+      <StRow className="direction-col ">
+        <h1>댓글 0개</h1>
+        <ul>
+          <li>현재 댓글기능은 지원하지 않습니다.</li>
+        </ul>
+      </StRow>
     </StWrap>
   );
 }
 
 const StWrap = styled.div`
-  width: 100%;
-  &.row {
-    display: flex;
-    padding: ${({ theme }) => theme.spacing.base};
-    gap: ${({ theme }) => theme.spacing.base};
+  padding: ${({ theme }) => theme.spacing.lg};
+  border: 3px solid red;
+`;
+
+const StRow = styled.div`
+  display: flex;
+  margin-bottom: ${({ theme }) => theme.spacing.lg};
+  gap: ${({ theme }) => theme.spacing.base};
+
+  &.center {
+    justify-content: center;
+    align-items: center;
+  }
+
+  &.header {
+    justify-content: space-between;
+    font-size: ${({ theme }) => theme.fontSize.xl};
+  }
+
+  &.direction-col {
+    flex-direction: column;
+  }
+`;
+
+const StIconWrap = styled.span`
+  cursor: pointer;
+  &:hover {
+    color: red;
   }
 `;
 
 const StImgWrap = styled.figure`
-  height: 10rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 800px;
+  height: 600px;
+  text-align: center;
+  position: relative;
+  overflow: hidden;
+
+  &::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 150%;
+    height: 150%;
+    background-position: center;
+    background-image: url(${({ backgroundImg }) => backgroundImg});
+    z-index: -1;
+    filter: blur(30px) brightness(0.5);
+  }
+
+  & img {
+    height: 100%;
+  }
+`;
+
+const StImgControler = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  line-height: 0;
+  padding: 1rem;
+  font-size: ${({ theme }) => theme.fontSize.xxxl};
+  border-radius: 50%;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: ${({ theme }) => theme.color.white};
+  cursor: pointer;
+
+  &.left {
+    left: ${({ theme }) => theme.spacing.base};
+  }
+
+  &.right {
+    right: ${({ theme }) => theme.spacing.base};
+  }
 `;
 
 const StSubImgWrap = styled.figure`
-  width: 8rem;
-  height: 6rem;
-  justify-content: center;
-  align-items: center;
+  height: 100px;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  transition: ${({ theme }) => theme.animation.transition};
+  cursor: pointer;
+  filter: brightness(0.5);
 
   & img {
-    width: 100%;
+    width: auto;
+    height: 100%;
+  }
+
+  &:hover,
+  &.active {
+    filter: brightness(1);
   }
 `;
 
